f9c41f345cb5e534608266fd8ddea2ffa5286147
MinJunChoi <chlalswns200@naver.com>
2025-03-17 16:56:24 +0900

    add sample entity and test

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -1,28 +1,27 @@
 package study.datajpa.repository;
 
 import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import study.datajpa.entity.Member;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
 @Rollback(false)
-class MemberJpaRepositoryTest {
+class MemberRepositoryTest {
 
     @Autowired
-    MemberJpaRepository memberJpaRepository;
-
+    MemberRepository memberRepository;
 //    @Test
 //    public void testMember() {
-//        Member member = new Member("memberA");
-//        Member savedMember = memberJpaRepository.save(member);
-//        Member findMember = memberJpaRepository.find(savedMember.getId());
+//        Member member = new Member();
+//        Member savedMember = memberRepository.save(member);
+//        Member findMember =
+//                memberRepository.findById(savedMember.getId()).get();
 //        assertThat(findMember.getId()).isEqualTo(member.getId());
 //        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
 //        assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장
